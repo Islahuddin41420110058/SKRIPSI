99d21372795fe53d5ce479084a67aa3a595b5782
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-11-23 21:21:38 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -111,7 +111,7 @@ r.get('/classify/:S/:K', function(req, res, next) {
                     `STATUS:: ${status}`
             ); // to telegram
             
-            res.json({jres_})
+            res.json({jres, jres_})
         })
     })
 });
