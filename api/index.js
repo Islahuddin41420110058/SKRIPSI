487cb26d92590a58b20a76140530e9a480a3d5d5
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-12-03 23:55:10 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -107,13 +107,11 @@ r.get('/classify/:S/:K', function(req, res, next) {
 //             jres_.split("|");
             const suhu = parseFloat(req.params.S);
             const kelembaban = parseFloat(req.params.K);
-            import time;
+           
             bot.sendMessage(
                     2128268907, //msg.id
-                    `SUHU:: ${suhu}
-                     KELEMBABAN ${kelembaban}
-                     Kondisi:: ${status}`
-                     time.sleep(5);
+                    `SUHU:: ${suhu} KELEMBABAN ${kelembaban} KONDISI:: ${status}`
+                     delay(1000);
                      
             ); // to telegram
             
